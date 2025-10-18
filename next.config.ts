0d8525a6e41bba7a9a41bfea8cc444e097ddbdf4
Pedro Medeiros <pereira.medeiros2@academico.ifrn.edu.br>
2025-10-18 16:41:23 -0300

    mudanças temporárias em editais/page.tsx

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: { //Permite tirar o ícone do Next durante o teste no navegador
-    buildActivity: false
-  },
   
   images: {
     remotePatterns: [
--- src/app/editais/page.tsx
+++ src/app/editais/page.tsx
@@ -1,5 +1,5 @@
-
 "use client";
+export const dynamic = 'force-dynamic';
 
 import { client } from "@/lib/sanity.client";
 import groq from "groq";
