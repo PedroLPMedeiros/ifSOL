c4875bdd0904e12438425e931f1fa8da62076cbf
PedroLPMedeiros <pereira.medeiros2@academico.ifrn.edu.br>
2025-10-27 13:29:56 -0300

    mudanças em NewsCard para alterar a altura do h3

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -30,7 +30,7 @@ export function NewsCard({ post }: NewsCardProps) {
           </div>
         )}
         <div className="p-4">
-          <h3 className="text-lg font-bold mb-2 text-gray-800 group-hover:text-green-700 transition-colors">{post.title}</h3>
+          <h3 className="text-lg font-bold mb-2 text-gray-800 group-hover:text-green-700 transition-colors line-clamp-3 h-12">{post.title}</h3> 
           <p className="text-xs text-gray-500 line-clamp-1">{post.description}</p>
         </div>
       </div>
