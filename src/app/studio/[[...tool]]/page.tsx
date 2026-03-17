d940f2f8869ec4918f5859952e59155a3886e9f8
PedroLPMedeiros <pereira.medeiros2@academico.ifrn.edu.br>
2026-03-17 14:09:08 -0300

    Fix: add generatestaticparams to studio page

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,12 @@
+export const dynamic = 'force-static'
+export function generateStaticParams() {
+  return[{ tool: []}];
+}
 
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
-export const dynamic = 'force-static'
+//export const dynamic = 'force-static'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
