e466abd22c06361b9a980ef9eb28b1657e897943
PedroLPMedeiros <pereira.medeiros2@academico.ifrn.edu.br>
2026-03-17 13:37:58 -0300

    Ajuste de config para deploy no Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+
+  output: 'export', //mudança para o github pages
+  basePath: '/ifSOL', //mudança para o github pages
   
   images: {
+    unoptimized: true,  //mudança para github pages, para não dar erro de build, já que o github pages não suporta imagens otimizadas
     remotePatterns: [
       {
         protocol: 'https',
